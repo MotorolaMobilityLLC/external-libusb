libusb/libusb

commit 8b46e1c088167eb86b1712765896e2f17d70d148

Windows: Add SetupAPI error handling
* http://msdn.microsoft.com/en-us/library/windows/hardware/ff545011.aspx
  states that SetupAPI errors must be be converted before passing it to FormatMessage().
* Use our own implementation of HRESULT_FROM_SETUPAPI to avoid defining a new function call.
* Issue and original fix suggested by Matthias Bolte
* Closes #166

libusb/version_nano.h
@@ -1,1 +1,1 @@
-#define LIBUSB_NANO 10856
+#define LIBUSB_NANO 10857

libusb/os/windows_usb.c
@@ -158,6 +158,20 @@ static char err_string[ERR_BUFFER_SIZE];
 
 	safe_sprintf(err_string, ERR_BUFFER_SIZE, "[%u] ", error_code);
 
+	// Translate codes returned by SetupAPI. The ones we are dealing with are either
+	// in 0x0000xxxx or 0xE000xxxx and can be distinguished from standard error codes.
+	// See http://msdn.microsoft.com/en-us/library/windows/hardware/ff545011.aspx
+	switch (error_code & 0xE0000000) {
+	case 0:
+		error_code = HRESULT_FROM_WIN32(error_code);	// Still leaves ERROR_SUCCESS unmodified
+		break;
+	case 0xE0000000:
+		error_code =  0x80000000 | (FACILITY_SETUPAPI << 16) | (error_code & 0x0000FFFF);
+		break;
+	default:
+		break;
+	}
+
 	size = FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM, NULL, error_code,
 		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), &err_string[safe_strlen(err_string)],
 		ERR_BUFFER_SIZE - (DWORD)safe_strlen(err_string), NULL);

MotorolaMobilityLLC/external-libusb

commit 0cea312a707cc5dcad480381b11da3eb256be3f5

Windows: Fix a MinGW compilation issue
* Some MinGW platforms may not have FACILITY_SETUPAPI defined
* Issue introduced with 8b46e1c088167eb86b1712765896e2f17d70d148

libusb/os/windows_usb.h
@@ -40,6 +40,11 @@
 #define SPDRP_INSTALL_STATE	34
 #endif
 
+// Missing from MinGW
+#if !defined(FACILITY_SETUPAPI)
+#define FACILITY_SETUPAPI	15
+#endif
+
 #if defined(__CYGWIN__ )
 #define _stricmp stricmp
 // cygwin produces a warning unless these prototypes are defined

libusb/version_nano.h
@@ -1,1 +1,1 @@
-#define LIBUSB_NANO 10860
+#define LIBUSB_NANO 10861